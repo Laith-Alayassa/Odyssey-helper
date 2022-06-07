13310d267a15d45b9063fc2bd0a8a0aaebeffeb6
ShawermaLVR <60319236+Laith-Alayassa@users.noreply.github.com>
2022-06-07 16:32:34 -0500

    formats output properly

--- plumber.py
+++ plumber.py
@@ -21,14 +21,26 @@ with pdfplumber.open('items.pdf') as pdf:
     text = page.extract_text()
 for row in text.split('\n'):
     row = row.split()
+
     try:
         if is_date(row[-4]):
-            date = row[-4]
-            for item in row:
-                if len(item) == 9 and item.isnumeric():
-                    id = item
-            # print(row)
-            print(f"this is the id {id} and this is date {date} ")
-            # print(f"row 3 is {row[-6]} and date is {row[-4]}")
+            due_date = row[-4]
+            item_borrowed = ""
+            student = ''
+            # find student id its index in list
+            for element in row:
+                if len(element) == 9 and element.isnumeric():
+                    id_index = row.index(element)
+                    id = element
+            
+            # find item name
+            for i in range(0,id_index-2):
+                item_borrowed += row[i] + " "
+            
+            for i in range (id_index + 1, len(row) - 4):
+                student += row[i]
+                
+            print(f'\n The student: {student}, id num: {id} borrowed {item_borrowed} that is due {due_date} \n')
+
     except:
         pass
