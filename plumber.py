14d8edc7f7229055fad635d031c521ac26b713a7
ShawermaLVR <60319236+Laith-Alayassa@users.noreply.github.com>
2022-07-09 18:34:38 -0500

    checks if date is late

--- plumber.py
+++ plumber.py
@@ -4,6 +4,7 @@ from dateutil.parser import parse
 import os
 import sys
 import glob
+from datetime import date
 
 def find_latest_file():
     """finds the last file to be added to a folder (currently downloads folder)
@@ -31,41 +32,52 @@ def is_date(string, fuzzy=False):
         return False
 
 
+def is_late(item_date, today):
+    item_due = parse(item_date)
+    return item_due >= today
 
-wrong_inputs = ['Available:','Checked']
 
+def find_late():
+    wrong_inputs = ['Available:', 'Checked']
+    today = date.today().strftime("%d/%m/%Y")
+    today = parse(today)
 
-path_to_pdf = find_latest_file()
-with pdfplumber.open(path_to_pdf) as pdf:
-    for l,pg in enumerate(pdf.pages):
-        page  = pdf.pages[l]
-        text = page.extract_text()
-    for row in text.split('\n'):
-        row = row.split()
+    path_to_pdf = find_latest_file()
+    with pdfplumber.open(path_to_pdf) as pdf:
+        for l, pg in enumerate(pdf.pages):
+            page = pdf.pages[l]
+            text = page.extract_text()
+        for row in text.split('\n'):
+            row = row.split()
 
-        try:
-            if is_date(row[-4]) and row[0] not in wrong_inputs:
-                due_date = row[-4]
-                item_borrowed = ""
-                student = ''
-                # find student id its index in list
-                for element in row:
-                    if len(element) == 9 and element.isnumeric():
-                        id_index = row.index(element)
-                        id = element
-                
-                # find item name
-                for i in range(0,id_index-1):
-                    item_borrowed += row[i] + " "
-                
-                for i in range (id_index + 1, len(row) - 4):
-                    student += row[i]
-                    
-                print(f'\n The student: {student}, id num: {id} borrowed {item_borrowed} that is due {due_date}')
-                # print(row)
-                print('\n')
+            try:
+                # Some names are too long and intersect with the date (eg. Alonso03/04/2002)
+                # So I use the last 10 chars which are the the date
+                date_location = row[-4][-10:]
+                if is_date(date_location) and is_late(date_location, today) and row[0] not in wrong_inputs:
+                    due_date = row[-4]
+                    item_borrowed = ""
+                    student = ''
+                    # find student id its index in list
+                    for element in row:
+                        # finds ID based on length and type
+                        if len(element) == 9 and element.isnumeric():
+                            id_index = row.index(element)
+                            id = element
 
-        except:
-            pass
+                    # find item name
+                    for i in range(0, id_index - 1):
+                        item_borrowed += row[i] + " "
+
+                    # finds student name
+                    for i in range(id_index + 1, len(row) - 4):
+                        student += row[i]
+
+                    print(f'\n The student: {student}, id num: {id} borrowed {item_borrowed} that is due {due_date}')
+                    print('\n')
+            except:
+                pass
+
+find_late()
 
 
--- sandbox.py
+++ sandbox.py
@@ -1,2 +1,12 @@
 
-find_latest_file()
+from datetime import date
+from dateutil.parser import parse
+
+today = date.today().strftime("%d/%m/%Y")
+today = parse(today)
+def is_late(item_date, today):
+    item_due = parse(item_date)
+    print(f"the item is late {item_due >= today}")
+
+
+is_late("09/07/2022", today)
